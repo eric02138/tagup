bf2ebe3c6abf9ecf0bafa9c118c7289a0ccccfce
Eric Mattison <eric_mattison@vrtx.com>
2018-12-02 19:59:45 -0500

    returned settings

--- main/settings.py
+++ main/settings.py
@@ -137,7 +137,7 @@ STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'djangorestapi4tu', 'static'),
+    os.path.join(BASE_DIR, 'static'),
 )
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
